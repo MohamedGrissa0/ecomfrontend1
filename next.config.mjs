2bd7984427a605ac3167315e9bcb76b88d919cfe
MohamedGrissa0 <mouhamedgrissa0@gmail.com>
2024-12-17 10:07:42 +0100

    5

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['ecombackend-yn1k.onrender.com'],
-         // Allow images from localhost
-      },
-};
-
-export default nextConfig;
